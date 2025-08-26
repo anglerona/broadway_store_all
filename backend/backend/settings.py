7cb0b241a8c1eb1f1c66b25efbb2e0afe506ca87
Angelina <angelina03wu@gmail.com>
2025-08-26 17:24:59 -0400

    capitalization

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*3s$)f#s3ohldl^*+$9y)r!6)sb%ja2v@-tll3_3$lg46q3#@v'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
